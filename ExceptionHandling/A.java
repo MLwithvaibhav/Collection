6a244e7de5ac281faa3339c708926d9b250f95b3
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2026-08-15 23:17:06 +0530

    Added exception handling part 1

---
+++ ExceptionHandling/A.java
@@ -0,0 +1,30 @@
+package ExceptionHandling;
+
+public class A {
+
+    public static void main(String [] args){
+
+        int i = 3;
+        int j = 0;
+
+        int nums[] = new int[5];
+
+        try
+        {
+            j = 18/i;
+            System.out.println(nums[5]);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Cannot divide by zero" + e);
+            
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Stay in your limits" + e);
+        }
+        System.out.println(j);
+        System.out.println("Bye");
+    }
+    
+}

751a199b31358a5e87893b1f6a5d73b135a09503
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2026-08-28 22:57:12 +0530

    Added arraylist collections

---
+++ CollectionTelusko/Arraylist.java
@@ -0,0 +1,16 @@
+import java.util.Collection;
+
+public class Arraylist{
+
+    public static void main(String[] args) {
+        
+        Collection nums = new Arraylist();
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+
+        System.out.println(nums);
+    }
+}
